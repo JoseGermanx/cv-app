a01b42e47ace695794f1b763d73fadf675d6a91b
Jose G Martinez <jgermanmm@gmail.com>
2024-02-25 15:06:17 -0300

    Agrega página de proyectos con lista de productos

---
+++ src/app/ui/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
+    <p className="text-gray-500">
+      A web application made with by {" "}
+      <a
+        className="font-medium text-gray-800 underline transition-colors"
+        href="https://dev.betacode.cl"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        BetaCode
+      </a>
+    </p>
+    <p>BetaCode Servicios Digitales e Informaticos E.I.R.L</p>
+  </div>
+  )
+}
+
+export default Footer
