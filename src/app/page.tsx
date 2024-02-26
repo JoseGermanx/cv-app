01e1e20bc723512adbe6241b9745f88365dfff6f
Jose G Martinez <jgermanmm@gmail.com>
2024-02-26 00:51:48 -0300

    Actualizar estilos de la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <NavBar />
       <main className="flex min-h-screen flex-col items-center justify-between p-3">
         <Hero />
-        <div className=" w-2/3 ">
+        <div className="w-2/3">
         <TerminalController />
         </div>
         <TechStack />
