bdcc3e8cf2eab273d2356edfda8b548e8bc5c50e
Jose G Martinez <jgermanmm@gmail.com>
2024-02-25 23:32:36 -0300

    Actualizar componente Terminal***
    ***Actualizar componente Terminal para agregar funcionalidad de entrada y salida de comandos en la terminal.

--- src/app/ui/Terminal.tsx
+++ src/app/ui/Terminal.tsx
@@ -1,33 +1,50 @@
-
-'use client'
-import React, {useState, useMemo, useEffect} from 'react';
-import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+"use client";
+import React, { useState, useMemo, useEffect, useCallback } from "react";
+import Terminal, {
+  ColorMode,
+  TerminalOutput,
+  TerminalInput,
+} from "react-terminal-ui";
 
 const TerminalController = (props = {}) => {
-
-    const initialState = useMemo(
-        () => [
-          <TerminalOutput>Ingresa un comando</TerminalOutput>,
-          <TerminalOutput></TerminalOutput>
-        ],
-        []
-      )
+  const initialState = useMemo(
+    () => [
+      <TerminalOutput>...</TerminalOutput>,
+      <TerminalOutput></TerminalOutput>,
+    ],
+    []
+  );
 
   const [terminalLineData, setTerminalLineData] = useState(initialState);
   // Terminal has 100% width by default so it should usually be wrapped in a container div
 
-  useEffect(() => {
-    setTerminalLineData(initialState)
-  }, [initialState])
+//   const [terminalInputValue, setTerminalInputValue] = useState("");
 
-  
+  useEffect(() => {
+    setTerminalLineData(initialState);
+  }, [initialState]);
+
+  const handleSubmission = useCallback(
+    ({ terminalSubmit }: { terminalSubmit: string }) => {
+        terminalLineData.push(<TerminalOutput>Nuevo mensaje</TerminalOutput>);
+    },
+    [setTerminalLineData]
+  );
+
   return (
     <div className="container">
-      <Terminal name='Bienvenido a la terminal de jgxdev' colorMode={ ColorMode.Dark}  onInput={ terminalInput => console.log(`New terminal input received: '${ terminalInput }'`) }>
-        { terminalLineData }
+      <Terminal
+        name="Bienvenido a la terminal de jgxdev"
+        colorMode={ColorMode.Dark}
+        onInput={(terminalInput) =>{
+          console.log(`New terminal input received: '${terminalInput}'`)
+          setTerminalLineData([...terminalLineData, <TerminalOutput>{terminalInput}</TerminalOutput>])
+        }}
+      >
+        {terminalLineData}
       </Terminal>
     </div>
-  )
+  );
 };
 
 export default TerminalController;
