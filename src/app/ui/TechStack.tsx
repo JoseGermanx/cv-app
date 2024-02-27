5ba74e1c5c1eb546e0075fb6790b725340e8cf3b
Jose G Martinez <jgermanmm@gmail.com>
2024-02-27 18:54:40 -0300

    Agregar logos a los objetos en TechStack.tsx

--- src/app/ui/TechStack.tsx
+++ src/app/ui/TechStack.tsx
@@ -1,53 +1,63 @@
 import Image from 'next/image'
 
+//incluir en los objetos una propiedad que sea el logo desde https://simpleicons.org/
+
 const tecnologies = [
     {
         id: 1,
         name: 'React',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
         imageAlt: "React",
+        imageLogo: "https://simpleicons.org/icons/react.svg"
     },
     {
         id: 2,
         name: 'Next.js',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg',
         imageAlt: "Next.js",
+        imageLogo: "https://simpleicons.org/icons/nextdotjs.svg"
     },
     {
         id: 3,
         name: 'Tailwind CSS',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Tailwind_CSS_logo.svg',
         imageAlt: "Tailwind CSS",
+        imageLogo: "https://simpleicons.org/icons/tailwindcss.svg"
     },
     {
         id: 4,
         name: 'Node.js',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
         imageAlt: "Node.js",
+        imageLogo: "https://simpleicons.org/icons/nodedotjs.svg"
     },
     {
         id: 5,
         name: 'Express.js',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
         imageAlt: "Express.js",
+        imageLogo: "https://simpleicons.org/icons/express.svg"
     },
     {
         id: 6,
         name: 'MongoDB',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg',
         imageAlt: "MongoDB",
+        imageLogo: "https://simpleicons.org/icons/mongodb.svg"
     },
     {
         id: 7,
         name: 'PostgreSQL',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg',
         imageAlt: "PostgreSQL",
+        imageLogo: "https://simpleicons.org/icons/postgresql.svg"
     },
     {
         id: 10,
         name: 'AWS',
         imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg',
-        imageAlt: "AWS"
+        imageAlt: "AWS",
+        imageLogo: "https://simpleicons.org/icons/amazonaws.svg"
     }
 ]
 
@@ -62,10 +72,9 @@ const TechStack = () => {
                         <div key={tecnology.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-auto overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75">
                                 <img
-                                    src={tecnology.imageSrc}
+                                    src={tecnology.imageLogo}
                                     alt={tecnology.imageAlt}
-                                    width="50%"
-                                    height="auto"
+                                    className=" h-5 w-5 object-cover object-center lg:h-20 lg:w-20"
                                 />
                             </div>
                         </div>
