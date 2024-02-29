d1c4acf047c4f7daf0b7263f3b92715e833e34df
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-29 18:21:19 -0300

    Añadir clase CSS para resaltar elementos en la lista de artículos

--- src/app/ui/ArticleListItems.tsx
+++ src/app/ui/ArticleListItems.tsx
@@ -9,7 +9,7 @@ const ArticleListItems = ({ articles }: Props) => {
     return (
         <div className="article-list mt-3">
             {articles.map((article, index) => (
-                <div key={index}>
+                <div key={index} className=" rounded hover:bg-gray-300">
                     <Link href={`/post/${article.id}`}>
                         {article.title}
                         <p><small>{article.date}</small></p>
