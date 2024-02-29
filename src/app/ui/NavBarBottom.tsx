fdfd78739ca95fde90b3197fe2b66d91733241f0
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-29 13:57:05 -0300

    Agregado enlace de inicio en la barra de navegación inferior

--- src/app/ui/NavBarBottom.tsx
+++ src/app/ui/NavBarBottom.tsx
@@ -7,6 +7,7 @@ const NavBarBottom = () => {
     <>
         <div className="bg-gray-900 p-4 text-white text-center">
             <div className="flex justify-center">
+            <a href="/" className="px-4">Inicio</a>
             <a href="/blog" className="px-4">Blog</a>
             <a href="/cv_jose_martinez.pdf" className="px-4">CV</a>
             <a href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7078460407316635648" className="px-4">Newsletter</a>
