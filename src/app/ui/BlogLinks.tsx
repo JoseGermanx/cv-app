708d9b0d376d4af0c0229e021ae7d74204d5d13c
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-29 15:34:20 -0300

    Actualizar BlogLinks.tsx para mostrar una línea divisoria entre los elementos de la lista de artículos

--- src/app/ui/BlogLinks.tsx
+++ src/app/ui/BlogLinks.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import { getCategorisedArticles } from '../lib/article'
-import ArticleListItems from './ArticleListItems';
+import React from "react";
+import { getCategorisedArticles } from "../lib/article";
+import ArticleListItems from "./ArticleListItems";
 
 const BlogLinks = () => {
-    const articles = getCategorisedArticles();
+  const articles = getCategorisedArticles();
   return (
-    <div className='h-screen'>
-        {
-            Object.keys(articles).map((article, index) => (
-                <ArticleListItems
-                    key={index}
-                    articles={articles[article]}
-                />
-            ))
-        }
+    <div className="h-screen">
+      {Object.keys(articles).map((article, index) => (
+        <>
+          <ArticleListItems key={index} articles={articles[article]} />
+          <hr />
+        </>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BlogLinks
+export default BlogLinks;
