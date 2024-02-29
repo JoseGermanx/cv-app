aa76d44dddd7818f13b6425be6f00c4177eb6283
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-29 09:39:24 -0300

    Actualizar diseño de página de artículo

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -9,21 +9,25 @@ const Article = async ({ params }: { params: { slug: string } }) => {
   return (
     <div>
       <NavBar />
-      <div className="w-full flex-none md:w-64 mb-5 mx-20 mt-9">
+      <div className="w-full flex md:w-64 mb-5 mx-20 mt-9">
         <Link href="/blog">
           <ArrowLeftIcon className="h-6 w-6" />
-          <p>Back home</p>
+          <p>back</p>
         </Link>
       </div>
+      <br />
       <div className="flex flex-col">
         <div>
           <h1 className="text-4xl mx-20">{articleData.title}</h1>
         </div>
         <p className="mx-20">{articleData.date}</p>
       </div>
-        <hr />
-        <div className="w-full mt-8 px-20 pb-10">
-      <div className="article" dangerouslySetInnerHTML={{ __html: articleData.contentHtml }} />
+      <hr />
+      <div className="w-full mt-8 px-20 pb-10">
+        <div
+          className="article"
+          dangerouslySetInnerHTML={{ __html: articleData.contentHtml }}
+        />
       </div>
     </div>
   );
