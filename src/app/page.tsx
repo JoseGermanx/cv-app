36e3eb7066ce50d0c604bb7e9ee5e3fbc09aea44
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-29 12:22:24 -0300

    Actualizar página con carga diferida en el controlador de terminal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useEffect } from "react";
 import NavBar from "./ui/Navbar";
@@ -6,20 +6,26 @@ import Hero from "./ui/Hero";
 import Footer from "./ui/Footer";
 import TechStack from "./ui/TechStack";
 import TerminalController from "./ui/Terminal";
+import { Suspense } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
-    <NavBar />
+      <NavBar />
       <main className="flex min-h-screen flex-col items-center justify-between p-3">
         <Hero />
-        <div className=" hidden sm:inline sm:mt-56 lg:mt-1 lg:w-2/3 sm:w-4/5" id="terminal">
-        <TerminalController />
+        <div
+          className=" hidden sm:inline sm:mt-56 lg:mt-1 lg:w-2/3 sm:w-4/5"
+          id="terminal"
+        >
+          <Suspense fallback={<div>Loading...</div>}>
+            <TerminalController />
+          </Suspense>
         </div>
-        <TechStack />        
+        <TechStack />
       </main>
       <Footer />
     </>
