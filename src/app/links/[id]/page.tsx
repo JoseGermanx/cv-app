52570555484adf4c5539636636b9bf45ee3699e2
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-04 20:39:34 -0300

    Eliminar importación innecesaria de Link en página de enlaces

--- src/app/links/[id]/page.tsx
+++ src/app/links/[id]/page.tsx
@@ -1,6 +1,5 @@
 
 import { LinksDeInteres } from '@/app/ui/LinksInteres'
-import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function LinkPage({ params }: { params: any }) {
