35af11e879ae31b36f2a8743d80323bd302a32b9
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-04 19:16:02 -0300

    Eliminar console.log y redirigir a la página principal si no se encuentra el enlace

--- src/app/links/[id]/page.tsx
+++ src/app/links/[id]/page.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function LinkPage({ params }: { params: any }) {
-    console.log(LinksDeInteres);
-    console.log(params);
+    
     const link = LinksDeInteres.find((link) => link.namebetacode === params.id);
-    console.log(link)
+    
     if (link) {
         redirect(link.path)
     }
-}   
+    redirect('/')
+}  
