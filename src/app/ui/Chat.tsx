85e6e41185f7e8dac499b873214f597f41ed587c
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-04 21:02:09 -0300

    Agregadas dependencias de socket.io-client y socket.io-parser

---
+++ src/app/ui/Chat.tsx
@@ -0,0 +1,52 @@
+import './App.css';
+import { io } from 'socket.io-client';
+import { useState, useEffect } from 'react';
+
+const socket = io('http://localhost:4000');
+
+function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [nuevoMensaje, setNuevoMensaje] = useState('');
+  const [mensajes, setMensajes] = useState([]);
+
+useEffect(() => {
+
+    socket.on('connect', () => setIsConnected(true));
+
+    socket.on('chat_message', (data) => {
+        setMensajes((mensajes): []never => [...mensajes, data]);
+    });
+
+    return () => {
+        socket.off('connect');
+        socket.off('chat_message');
+    }
+
+}, []);
+
+  const enviarMensaje = () => {
+    socket.emit('chat_message', {
+      usuario: socket.id,
+      mensaje: nuevoMensaje
+    });
+  }
+
+  return (
+    <div className="App">
+      <h2>{isConnected ? 'CONECTADO' : 'NO CONECTADO'}</h2>
+      <ul>
+        {mensajes.map(mensaje => (
+          <li>{ key={mensaje.id} mensaje.usuario}: {mensaje.mensaje}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onChange={e => setNuevoMensaje(e.target.value)}
+      />
+      <button onClick={enviarMensaje}>Enviar</button>
+    </div>
+  );
+}
+
+export default App;
