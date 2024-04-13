2e4bcfc9f16db9bf7aad5b54cf5addfe279836b6
Jose Germán Martínez <jgermanmm@gmail.com>
2024-04-12 22:59:20 -0400

    Eliminar enlace y logotipo de NERDEARLA en Hero.tsx

--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -39,15 +39,6 @@ const Hero = () => {
         />
       </div>
       <div className="mx-auto max-w-2xl py-4 sm:py-4 lg:py-4">
-        <Link href="/post/nerdearla-cl-2024">
-          <Image
-            className="h-auto w-100"
-            src="/images/NERDEARLA_logos-01.png"
-            alt="NERDEARLA"
-            width={1200}
-            height={200}
-          />
-        </Link>
         <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
