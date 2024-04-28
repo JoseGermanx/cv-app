c69bc09e1ba13213cff6d3e91167018e81c0ab2e
Jose G Martinez <jgermanmm@gmail.com>
2024-04-28 19:27:22 -0400

    function generateStaticParams

--- src/app/links/[id]/page.tsx
+++ src/app/links/[id]/page.tsx
@@ -2,6 +2,16 @@
 import { LinksDeInteres } from '@/app/ui/LinksInteres'
 import { redirect } from 'next/navigation'
 
+// incluir generateStaticParams()
+
+export async function generateStaticParams() {
+    return LinksDeInteres.map((link) => ({
+        params: {
+            id: link.namebetacode
+        }
+    }))
+}
+
 export default async function LinkPage({ params }: { params: any }) {
     
     const link = LinksDeInteres.find((link) => link.namebetacode === params.id);
