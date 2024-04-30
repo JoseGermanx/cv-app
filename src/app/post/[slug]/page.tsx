8aa36217b2b613d2a04dc57be8f43553a503457e
Jose G Martinez <jgermanmm@gmail.com>
2024-04-30 00:04:56 -0400

    Actualizar generación de parámetros estáticos en page.tsx

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -5,14 +5,12 @@ import NavBar from "@/app/ui/Navbar";
 import Footer from "@/app/ui/Footer";
 
 export function generateStaticParams() {
-const slug = getSortedrArticles()
-  return slug.map((slug) => {
-    return {
-      params: {
-        slug: slug.id,
-      },
-    };
-  });
+  const slug = getSortedrArticles();
+  return slug.map((slug) => ({
+    params: {
+      slug: slug.id,
+    },
+  }));
 }
 
 const Article = async ({ params }: { params: { slug: string } }) => {
@@ -20,30 +18,30 @@ const Article = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <>
-    <div>
-      <NavBar />
-      <div className="flex md:w-64 mb-5 mx-20 mt-9">
-        <Link href="/blog">
-          <ArrowLeftIcon className="h-6 w-6" />
-          <p>back</p>
-        </Link>
-      </div>
-      <br />
-      <div className="mx-10 sm:mx-20">
-        <div>
-          <h1 className="text-4xl font-bold ">{articleData.title}</h1>
+      <div>
+        <NavBar />
+        <div className="flex md:w-64 mb-5 mx-20 mt-9">
+          <Link href="/blog">
+            <ArrowLeftIcon className="h-6 w-6" />
+            <p>back</p>
+          </Link>
         </div>
-        <p className="">{articleData.date}</p>
+        <br />
+        <div className="mx-10 sm:mx-20">
+          <div>
+            <h1 className="text-4xl font-bold ">{articleData.title}</h1>
+          </div>
+          <p className="">{articleData.date}</p>
+        </div>
+        <hr />
+        <div className="mt-8 mx-10 p-15 sm:px-25 pb-10">
+          <div
+            className="article"
+            dangerouslySetInnerHTML={{ __html: articleData.contentHtml }}
+          />
+        </div>
       </div>
-      <hr />
-      <div className="mt-8 mx-10 p-15 sm:px-25 pb-10">
-        <div
-          className="article"
-          dangerouslySetInnerHTML={{ __html: articleData.contentHtml }}
-        />
-      </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
