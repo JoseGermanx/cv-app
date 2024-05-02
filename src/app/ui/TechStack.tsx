42aa135e28f481411d90dd2ad6f2ecbfea39b4bf
Jose G Martinez <jgermanmm@gmail.com>
2024-05-02 13:33:57 -0400

    Actualizar estilos en la lista de artículos

--- src/app/ui/TechStack.tsx
+++ src/app/ui/TechStack.tsx
@@ -150,7 +150,7 @@ const TechStack = () => {
             <div className=" mt-24 mx-auto max-w-2xl px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="text-5xl font-bold tracking-tight text-gray-900 text-center mb-5">Tecnologías</h2>
 
-                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 justify-center ">
+                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-8 xl:gap-x-8 justify-center ">
                     {tecnologies.map((tecnology) => (
                         <div key={tecnology.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-auto overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75">
