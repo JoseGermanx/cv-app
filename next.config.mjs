a83f297d436e745da4a284bb3f221817db277565
Jose G Martinez <jgermanmm@gmail.com>
2024-05-03 23:32:08 -0400

    Actualizar configuración de variables de entorno en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    // NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
-    // NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    // NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
-  },
     reactStrictMode: true,
     images: {
         remotePatterns: [
