207b5b0d93265d5e8b04b132ae26b9e468ee0484
Jose G Martinez <jgermanmm@gmail.com>
2024-05-10 06:58:26 -0400

    validacion de dispositivo movil

--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -21,6 +21,10 @@ const NavBar = () => {
   }
 
   useEffect(()=>{
+    let navegador = navigator.userAgent;
+        if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+            return 
+        }
     if (Notification.permission === "granted") {
       setNotificaciones(true)
     }
