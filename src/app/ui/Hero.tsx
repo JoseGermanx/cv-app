5b4fd6bc3cb839a6d590265c296a627a55e8595c
Jose Germán Martínez <jgermanmm@gmail.com>
2024-05-11 23:11:10 -0400

    Actualizar estilos de la página de inicio para mejorar rendimiento de carga

--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
   return (
     <div className="relative isolate px-6 pt-2 lg:px-8 h-auto mt-20 mb-10">
       <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        className="absolute inset-x-0 -top-40 -z-20 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         aria-hidden="true"
       >
         <div
