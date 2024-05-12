cc2d330485fc9abe12c56e7aa2f4dca4d10d2c7e
Jose Germán Martínez <jgermanmm@gmail.com>
2024-05-11 22:55:52 -0400

    feat: Actualizar página de inicio para mejorar rendimiento de carga

--- src/app/ui/NavBarBottom.tsx
+++ src/app/ui/NavBarBottom.tsx
@@ -1,5 +1,6 @@
 // navbar para el footer, el cual se encuentra en la parte inferior de la pagina. Links solo texto y en horizontal.
 
+import Link from 'next/link'
 import React from 'react'
 
 const NavBarBottom = () => {
@@ -7,10 +8,10 @@ const NavBarBottom = () => {
     <>
         <div className="bg-gray-900 p-4 text-white text-center">
             <div className="flex justify-center">
-            <a href="/" className="px-4">Inicio</a>
-            <a href="/blog" className="px-4">Blog</a>
-            <a href="/cv_jose_martinez.pdf" className="px-4">CV</a>
-            <a href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7078460407316635648" className="px-4">Newsletter</a>
+            <Link href="/" className="px-4">Inicio</Link>
+            <Link href="/blog" className="px-4">Blog</Link>
+            <Link href="/cv_jose_martinez.pdf" className="px-4">CV</Link>
+            <Link href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7078460407316635648" className="px-4">Newsletter</Link>
             </div>
         </div>
     </>
