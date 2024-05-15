1e481fbf7d09b7a8115ab0e4bb2ced5588ea8bca
Jose G Martinez <jgermanmm@gmail.com>
2024-05-14 23:56:57 -0400

    Actualizar clase de estilo para enlaces en LinksInteres.tsx

--- src/app/ui/TechStack.tsx
+++ src/app/ui/TechStack.tsx
@@ -175,7 +175,7 @@ const TechStack = () => {
                     Estas son algunas de las tecnologías que he utilizado en mis proyectos.
                 </p>
                 <h3 className="text-3xl mb-3 mt-3">Front end</h3>
-                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-8 xl:gap-x-8 justify-center ">
+                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 md:grid-cols-8 xl:gap-x-8 justify-center ">
                     {tecnologies.filter(tec => tec.focus === "Frontend").map((tecnology) => (
                         <div key={tecnology.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-auto overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75">
@@ -192,7 +192,7 @@ const TechStack = () => {
                 </div>
                 <hr className="mt-10" />
                 <h3 className="text-3xl mb-3 mt-3">Back end</h3>
-                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-8 xl:gap-x-8 justify-center ">
+                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 md:grid-cols-8 xl:gap-x-8 justify-center ">
                     {tecnologies.filter(tec => tec.focus === "Backend").map((tecnology) => (
                         <div key={tecnology.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-auto overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75">
@@ -209,7 +209,7 @@ const TechStack = () => {
                 </div>
                 <hr className="mt-10" />
                 <h3 className="text-3xl mb-3 mt-3">Lenguajes y herramientas</h3>
-                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-8 xl:gap-x-8 justify-center ">
+                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 md:grid-cols-8 xl:gap-x-8 justify-center ">
                     {tecnologies.filter(tec => tec.focus === "Programming").map((tecnology) => (
                         <div key={tecnology.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-auto overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75">
@@ -226,7 +226,7 @@ const TechStack = () => {
                 </div>
                 <hr className="mt-10" />
                 <h3 className="text-3xl mb-3 mt-3">DevOps y operaciones</h3>
-                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-8 xl:gap-x-8 justify-center ">
+                <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-3 md:grid-cols-8 xl:gap-x-8 justify-center ">
                     {tecnologies.filter(tec => tec.focus === "DevOps").map((tecnology) => (
                         <div key={tecnology.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-auto overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75">
