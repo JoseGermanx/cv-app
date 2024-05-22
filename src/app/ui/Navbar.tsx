14b31297712570770e6136f3b53aaee647a10284
Jose G Martinez <jgermanmm@gmail.com>
2024-05-22 15:39:52 -0400

    chore: Update npm dependencies and add new packages

--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+// import  Menu  from "./Menu";
 
 const NavBar = () => {
   const [notificaciones, setNotificaciones] = useState (false);
@@ -135,6 +136,11 @@ const NavBar = () => {
                 >
                   Blog
                 </Link>
+                {/* <span
+                 className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                <Menu
+                />
+                </span> */}
               </div>
             </div>
           </div>
