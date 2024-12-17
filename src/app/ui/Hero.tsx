adf22010ed23fcfb8ae72898258f80c623317b8e
Jose Germán Martínez <jgermanmm@gmail.com>
2024-12-17 15:47:56 -0300

    mejorar texto de hero

--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -55,8 +55,8 @@ const Hero = () => {
             <span ref={el} className=" text-gray-600"></span>
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Hola, bienvenido a mi sitio web. Mi nombre es José Germán. Soy ingeniero, desarrollador de
-            Software Full Stack e instructor de JavaScript.
+            Mi nombre es José Germán, tengo +10 años de experiencia como desarrollador web y actualmente ayudo a estudiantes
+            de desarrollo Full Stack en JavaScript.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             {/* <Link href="/projects" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
